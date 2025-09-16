9812bb0fd987fe86ca49fcd944b71295b88f4269
samyaw-li <samuelli@g.ucla.edu>
2025-09-16 02:25:43 -0400

    Updated README, added github link

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Work in progress...</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,6 +24,13 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <a
+          href="https://github.com/samyaw-li"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View my GitHub
+        </a>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
